a07423c5b66c1cca779a4d581cc767d294a8beaa
Nazim Asadov <nazimatmu@gmail.com>
2022-02-06 01:28:28 +0400

    Initial Commit

---
+++ SnapchatCloneNZM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatCloneNZM
+//
+//  Created by Nazim Asadov on 06.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
